68b7a0e44bce1315b934da03e8dadeb6c4cd0326
sidohin-felix <sidohin.felix@gmail.com>
2022-09-20 19:25:51 +0300

    added endpoints

--- src/main/java/io/trustody/assetlibrary/api/AdminApi.java
+++ src/main/java/io/trustody/assetlibrary/api/AdminApi.java
@@ -1,28 +1,67 @@
 package io.trustody.assetlibrary.api;
 
-import io.trustody.assetlibrary.persistence.AssetRepository;
+import ammer.tech.commons.ledger.entities.assets.BaseAsset;
+import ammer.tech.commons.ledger.entities.assets.Network;
+import ammer.tech.commons.ledger.entities.assets.SmartAsset;
+import com.jsoniter.JsonIterator;
+import com.jsoniter.output.JsonStream;
+import io.trustody.assetlibrary.persistence.NetworkRepository;
+import io.trustody.assetlibrary.persistence.SmartAssetRepository;
 import jakarta.inject.Inject;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.ws.rs.DELETE;
-import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.Response;
+
+import java.util.UUID;
 
 @Path("/api/admin")
 public class AdminApi {
 
     @Inject
-    private AssetRepository assetRepository;
+    private NetworkRepository networkRepository;
+    @Inject
+    private SmartAssetRepository smartAssetRepository;
 
-    @POST
-    public Response createOrModifyAsset(@Context HttpServletRequest request){
-       return null;
+    @PUT
+    @Path("/networks/add")
+    public Response appendNetwork(@Context HttpServletRequest request) throws Exception {
+        Network network = JsonIterator.deserialize(request.getInputStream().readAllBytes(), Network.class);
+        return Response.ok(JsonStream.serialize(networkRepository.upsertElement(network))).build();
     }
 
     @DELETE
-    public Response addAsset(){
-        return Response.noContent().build();
+    @Path("/networks/delete/{networkId}")
+    public Response deleteNetwork(@PathParam("networkId") String networkId) {
+        if (networkRepository.deleteElement(Network.builder().id(UUID.fromString(networkId)).build()))
+            return Response.ok().build();
+        else return Response.serverError().build();
     }
 
+    @PUT
+    @Path("/assets/add")
+    public Response appendAsset(@Context HttpServletRequest request) throws Exception {
+        byte[] objectBytes = request.getInputStream().readAllBytes();
+        SmartAsset smartAsset = JsonIterator.deserialize(objectBytes, SmartAsset.class);
+        switch (smartAsset.getAssetType()) {
+            case ERC20:
+                break;
+            case ERC721:
+                break;
+            case ERC1155:
+                break;
+        }
+        return Response.notModified().build();
+    }
+
+    @DELETE
+    @Path("/assets/delete/{assetId}")
+    public Response deleteAsset(@PathParam("assetId") String assetId) {
+        if (smartAssetRepository.deleteElement(SmartAsset.builder().id(UUID.fromString(assetId)).build()))
+            return Response.ok().build();
+        else return Response.serverError().build();
+    }
 }
--- src/main/resources/WEB-INF/shared/api.js
+++ src/main/resources/WEB-INF/shared/api.js
@@ -1,16 +1,29 @@
 async function submitCreateNetwork()
 {
-    var data = new FormData();
-    data.append("email",document.getElementById("networKname"));
-    //continued.
-    var res = await fetch();
-    //Check for 200 and then get the result and pain the canvas.
-    //If not 200 then show error dialog.
+    var data = {};
+    data["id"] = document.getElementsByName("id")[0].value;
+    data["networkId"] = document.getElementsByName("networkId")[0].value;
+    data["networkName"] = document.getElementsByName("networkName")[0].value;
+    data["visibleName"] = document.getElementsByName("visibleName")[0].value;
+    data["iconUrl"] = document.getElementsByName("iconUrl")[0].value;
+    data["explorerTransactions"] = document.getElementsByName("explorerTransactions")[0].value;
+    data["explorerAddress"] = document.getElementsByName("explorerAddress")[0].value;
+    data["compatiblePubKeys"] = document.getElementsByName("compatiblePubKeys")[0].value;
+    data["visible"] = document.getElementsByName("visible")[0].checked;
+    const options = {method: 'POST',body: JSON.stringify(data),headers: { 'Content-Type':'application/json' }}
+    var res = await fetch('/createNetwork', options);
+    if(res.status !== 200){
+        alert('The server returned an error - could not save the network!');
+        return;
+    }
+    var json = await res.json()
+    alert('Successfully saved the new network!')
 }
 
 async function submitCreateAsset()
 {
     var data = new FormData();
+
     data.append("email",document.getElementById("networKname"));
     //continued.
     //if ERC20 then ignore the sub-tokens.
@@ -21,11 +34,29 @@ async function submitCreateAsset()
     //If not 200 then show error dialog.
 }
 
+async function getAssetByIdAndType()
+{
+    //Get a specific asset.
+    var res = await fetch();
+}
+
+async function getNetworkById()
+{
+    //Get a specific asset.
+    var res = await fetch();
+}
+
 
 async function listNetworks()
 {
-    //Use fetch api to get the list of all networks.
-    var res = await fetch();
+    const options = {method: 'GET'}
+    var res = await fetch('/api/public/networks', options);
+    if(res.status !== 200){
+        alert('Could not fetch the list of networks!');
+        return;
+    }
+    var json = await res.json()
+    alert(json)
 }
 
 async function listAssets()
